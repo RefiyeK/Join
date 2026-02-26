e786632176a9670a20b0444d8c8681332aa279a3
Philipp Biebert <philipp.biebert@gmail.com>
2026-02-26 14:43:20 +0100

    feat(sign-up): Add validation for registration form
    - Implemented validation logic for name, email, and password in the sign-up form
    - Added error messages for invalid inputs in the HTML
    - Integrated password confirmation and privacy policy checkbox
    - Disabled submit button if validation fails or privacy policy is not accepted

---
+++ src/app/interfaces/new-user.ts
@@ -0,0 +1,7 @@
+export interface NewUser {
+  name: string;
+  email: string;
+  passwort: string;
+  confirmPassword: string;
+  privacyPolicy: boolean;
+}
--- src/app/pages/sign-up/sign-up.ts
+++ src/app/pages/sign-up/sign-up.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NewUser } from '../../interfaces/new-user';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.scss',
 })
-export class SignUp {}
+export class SignUp implements OnInit {
+  checkPrivacyPolicy = false;
+
+  newUser: NewUser = {
+    name: '',
+    email: '',
+    passwort: '',
+    confirmPassword: '',
+    privacyPolicy: true,
+  };
+
+  ngOnInit(): void {
+    this.setInitalValues();
+  }
+
+  setInitalValues() {
+    this.newUser = {
+      name: 'test',
+      email: 'test@test.de',
+      passwort: '12345678',
+      confirmPassword: '12345678',
+      privacyPolicy: false,
+    };
+  }
+
+  onSubmit() {
+    // this.setUserData();
+    console.log(this.newUser);
+    this.clearInput();
+  }
+
+  clearInput() {
+    this.newUser.name = '';
+    this.newUser.email = '';
+    this.newUser.passwort = '';
+    this.newUser.confirmPassword = '';
+    this.newUser.privacyPolicy = true;
+  }
+
+  setPrivacyPolicy() {
+    if (this.newUser.privacyPolicy) {
+      this.newUser.privacyPolicy = false;
+      this.checkPrivacyPolicy = false;
+    } else {
+      this.newUser.privacyPolicy = true;
+    }
+  }
+}
